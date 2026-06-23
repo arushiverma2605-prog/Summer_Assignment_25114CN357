72ff036888cceaa66c3986880cc1ba89cd604b47
arushiverma2605-prog <arushi.verma2605@gmail.com>
2026-06-23 21:43:41 +0530

    Add files via upload
    DAY10

---
+++ ques38.java
@@ -0,0 +1,16 @@
+public class ques38 {
+    public static void main(String[] args) {
+        for(int i=5;i>=1;i--)
+        {
+            for(int j=1;j<=5-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=i*2-1;j>=1;j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
